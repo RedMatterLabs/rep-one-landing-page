c29183c15330e3aab98709f35416d997f597eebf
Gregory Mender <Greg@Gregorys-MacBook-Pro.local>
2018-03-30 03:49:11 -0300

    fixed footer

--- src/components/App/Footer/index.js
+++ src/components/App/Footer/index.js
@@ -6,41 +6,41 @@ class Footer extends React.Component {
     return (
       <div className={styles.footer}>
         <div className={styles.main}>
-            <div>
-              <h2>Contact Us</h2>
-              <ul className={styles.adress}>
-                <li>275 Brooklyn St.</li>
-                <li>Williamsburg, NY, 12345</li>
-                <li>United States</li>
-              </ul>
-              <br />
-              <a href="mailto:squatsandscience@gmail.com">squatsandscience@gmail.com</a>
-            </div>
-            <div className={styles.footerSocialIcons}>
-              <ul className={styles.socialIcons}>
-                <li>
-                  <a href="" className={styles.socialIcon}>
-                    {' '}<i className="icon fa fa-github" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className={styles.socialIcon}>
-                    {' '}<i className="icon fa fa-facebook" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className={styles.socialIcon}>
-                    {' '}<i className="icon fa fa-instagram" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className={styles.socialIcon}>
-                    {' '}<i className="icon fa fa-twitter" />
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div>
+            <h2>Contact Us</h2>
+            <ul className={styles.adress}>
+              <li>415 Wythe Ave</li>
+              <li>Brooklyn, NY 11249</li>
+              <li>United States</li>
+            </ul>
+            <br />
+            <a href="mailto:squatsandscience@gmail.com">squatsandscience@gmail.com</a>
           </div>
+          <div className={styles.footerSocialIcons}>
+            <ul className={styles.socialIcons}>
+              <li>
+                <a href="" className={styles.socialIcon}>
+                  {' '}<i className="icon fa fa-github" />
+                </a>
+              </li>
+              <li>
+                <a href="" className={styles.socialIcon}>
+                  {' '}<i className="icon fa fa-facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="" className={styles.socialIcon}>
+                  {' '}<i className="icon fa fa-instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="" className={styles.socialIcon}>
+                  {' '}<i className="icon fa fa-twitter" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
