298563b44261a265c4ccb8bbcbe79cf8fda361a0
JonnyLinja <jonny.linja@gmail.com>
2018-04-09 17:26:23 -0400

    twaeked env

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'react-router/lib/Router';
 import browserHistory from 'react-router/lib/browserHistory';
-import routes from '../routes';
+import routes from './routes';
 
 // We need a Root component for React Hot Loading.
 function Root() {
