f9d8876437f85e6e8098fd845d2e105414017962
JonnyLinja <jonny.linja@gmail.com>
2018-04-12 18:46:55 -0400

    remove unused components

--- src/components/App/Routes/Home/CTA/components/pricing/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CTA from './';
-
-it('Test example', () => {
-  const wrapper = shallow(<CTA />);
-  expect(wrapper.is('div')).toBeTruthy();
-});
