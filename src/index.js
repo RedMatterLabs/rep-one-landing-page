e7cac7a4b42a0e9e0a375db4db8d362f4e4688b6
JonnyLinja <jonny.linja@gmail.com>
2018-04-14 00:30:38 -0400

    basic GA setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './Root';
 import "react-hot-loader/patch";
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-117495949-1');
 const root = document.querySelector('#root');
 
 const mount = RootComponent => {
