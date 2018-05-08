090125a4ee7cfc4d31b6721b6e9319375988ca52
JonnyLinja <jonny.linja@gmail.com>
2018-05-07 21:49:43 -0400

    hash scroll fix

--- src/Root.js
+++ src/Root.js
@@ -5,7 +5,21 @@ import routes from './routes';
 
 // We need a Root component for React Hot Loading.
 function Root() {
-  return <Router history={browserHistory} routes={routes} />;
+  return <Router history={browserHistory} routes={routes} onUpdate={hashLinkScroll} />;
+}
+
+function hashLinkScroll() {
+  const { hash } = window.location;
+  if (hash !== '') {
+    // Push onto callback queue so it runs after the DOM is updated,
+    // this is required when navigating from a different page so that
+    // the element is rendered on the page before trying to getElementById.
+    setTimeout(() => {
+      const id = hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) element.scrollIntoView();
+    }, 500);
+  }
 }
 
 export default Root;
