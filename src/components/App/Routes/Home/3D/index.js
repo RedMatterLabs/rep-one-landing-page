ef7c560ad0ba2e2f30c03afde4cd034056e9ba15
JonnyLinja <jonny.linja@gmail.com>
2018-05-29 18:30:10 -0400

    attempt at redrawing the canvas, this is not working well

--- src/components/App/Routes/Home/3D/index.js
+++ src/components/App/Routes/Home/3D/index.js
@@ -5,6 +5,32 @@ import sketch from './sketch';
 
 class ThreeDee extends Component {
 
+  componentDidMount() {
+    var that = this;
+    setTimeout(() => {
+      var canvas = document.getElementById('defaultCanvas0');
+      window.addEventListener('resize', resizeCanvas, false);
+      
+      function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when 
+         * you resize the browser window and the canvas goes will be cleared.
+         */
+        drawStuff(); 
+      }
+      resizeCanvas();
+
+      function drawStuff() {
+        console.log('should redraw');
+        that.forceUpdate();
+      }
+
+    }, 3000);
+  }
+
   render() {
     return (
       <div className={styles.section}>
