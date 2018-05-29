0d54390f3d150111e236f1ef7a23c988f9c4fc9f
JonnyLinja <jonny.linja@gmail.com>
2018-05-29 14:43:14 -0400

    clicking inside modal no longer dismisses it

--- src/components/App/Shared/Modal/index.js
+++ src/components/App/Shared/Modal/index.js
@@ -1,26 +1,34 @@
 import React, {Component} from 'react';
 import styles from './styles.scss';
+import onClickOutside from "react-onclickoutside";
 
 class Modal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      top: 1,
-      height: -1,
-      scrolling: false,
-    };
-  };
 
   render() {
-
     let style = (this.props.open) ? {display: 'flex'} : {display: 'none'};
-    
+    let InnerModalWrapped = onClickOutside(InnerModal)
+
     return (
-      <div onClick={this.props.closeModal} className={styles.modalcontainer} style={style} >
-        <div className={styles.modal}>
-          <a className={`${styles.closebutton} ${styles.button}`} onClick={this.props.closeModal}>X</a>
+      <div className={styles.modalcontainer} style={style} >
+        <InnerModalWrapped onClickOutside={this.props.closeModal} closeModal={this.props.closeModal}>
           {this.props.children}
-        </div>
+        </InnerModalWrapped>
+      </div>
+    );
+  }
+
+}
+
+class InnerModal extends Component {
+  handleClickOutside = evt => {
+    this.props.onClickOutside()
+  }
+
+  render() {
+    return (
+      <div className={styles.modal}>
+        <a className={`${styles.closebutton} ${styles.button}`} onClick={this.props.closeModal}>X</a>
+        {this.props.children}
       </div>
     );
   }
