62308d6e26094f219f5293889272c0003df07867
JonnyLinja <jonny.linja@gmail.com>
2018-05-29 18:10:06 -0400

    basic section for the 3d

--- src/components/App/Routes/Home/3D/index.js
+++ src/components/App/Routes/Home/3D/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles.scss';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 
-class ThreeDee extends React.Component {
+class ThreeDee extends Component {
 
   render() {
     return (
-      <div className={styles.section} id="3D">
-        <P5Wrapper sketch={sketch} />
+      <div className={styles.section}>
+        <div className={styles.main}>
+          <P5Wrapper className={styles.main} sketch={sketch} />
+        </div>
       </div>
     );
   }
