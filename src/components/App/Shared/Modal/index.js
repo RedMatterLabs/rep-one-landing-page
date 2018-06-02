cf959f9e93b6e5b27128b9d26c21e914fbe6ecbc
JonnyLinja <jonny.linja@gmail.com>
2018-06-02 18:59:44 -0400

    remove ability to exit idle modal

--- src/components/App/Shared/Modal/index.js
+++ src/components/App/Shared/Modal/index.js
@@ -1,37 +1,20 @@
 import React, {Component} from 'react';
 import styles from './styles.scss';
-import onClickOutside from "react-onclickoutside";
 
 class Modal extends Component {
 
   render() {
     let style = (this.props.open) ? {display: 'flex'} : {display: 'none'};
-    let InnerModalWrapped = onClickOutside(InnerModal)
 
     return (
       <div className={styles.modalcontainer} style={style} >
-        <InnerModalWrapped onClickOutside={this.props.closeModal} closeModal={this.props.closeModal}>
+        <div className={styles.modal}>
           {this.props.children}
-        </InnerModalWrapped>
+        </div>
       </div>
     );
   }
 
-}
-
-class InnerModal extends Component {
-  handleClickOutside = evt => {
-    this.props.onClickOutside()
-  }
-
-  render() {
-    return (
-      <div className={styles.modal}>
-        <a className={`${styles.closebutton} ${styles.button}`} onClick={this.props.closeModal}>X</a>
-        {this.props.children}
-      </div>
-    );
-  }
 }
 
 export default Modal;
