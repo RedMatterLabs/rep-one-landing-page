509aa7e7ad0a1bdb219cdd712367cbf351fcfee4
JonnyLinja <jonny.linja@gmail.com>
2018-06-20 19:53:04 -0400

    updated copy and made resizing of repone device based on div not window

--- src/components/App/Routes/Home/Insights/index.js
+++ src/components/App/Routes/Home/Insights/index.js
@@ -21,8 +21,7 @@ function Insights() {
 
         <div className={styles.mbtoverlay}>
           <div className={styles.description}>
-            Our unique 3D-sensing technology makes the invisible, visible. RepOne tracks everything from velocity to skill adaptation — even trends in motion that indicate fatigue and injury. Gain access to unmatched analysis of all your athletes’ movements and make your team stronger than ever.
-          </div>
+            Our unique 3D-sensing technology makes the invisible, visible. RepOne tracks everything from velocity to skill adaptation — even trends in motion that indicate fatigue and injury. <b>The new sensing also removes the need to keep a device directly underneath the weights.</b> Gain access to unmatched analysis of all your athletes’ movements and make your team stronger than ever.</div>
           <a href="http://blog.reponestrength.com/2018/05/07/the-emergence-of-motion-based-training/" className={styles.button}>Learn more about Motion Based Training</a>
         </div>
       </div>
